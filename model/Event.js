4e29ae27940421cadbb0dcdfcad9bd8986a8102d
James <JamesLatimerTaylor@gmail.com>
2025-07-13 17:17:12 +0100

    Updated event class

--- model/Event.js
+++ model/Event.js
@@ -1,3 +1,5 @@
+const { Location } = require('./User');
+
 const EventSize = Object.freeze({
   TINY: '5-10',
   SMALL: '10-20',
@@ -17,29 +19,33 @@ class Event {
   /**
    * @param {Object} options
    * @param {string} options.eventId
-   * @param {string} [options.addedBy] // UserId of the user who added this event
-   * @param {Date} options.date
    * @param {string} options.title
-   * @param {string} options.locationDescription // Description of the location
-   * @param {string} options.location
-   * @param {boolean} options.memberOnly
-   * @param {string} [options.externalRegister] // Link/email to tickets
+   * @param {string} [options.description]
+   * @param {Date} options.date
+   * @param {string|null} [options.startTime]
+   * @param {string} [options.duration] // e.g. '1h_or_less', '1_to_2', etc.
+   * @param {string} [options.locationAddress]
+   * @param {string} [options.locationPostcode]
+   * @param {Location} options.location // Location object
+   * @param {boolean} [options.memberOnly]
+   * @param {string} [options.externalRegister]  // URL for external registration
    * @param {boolean} [options.localMeetRegister]
    * @param {string[]} [options.groupTags]
    * @param {string[]} [options.categoryTags]
-   * @param {string} options.description
-   * @param {string} options.contactPerson
-   * @param {string} options.contactDetails
+   * @param {string} [options.contactPerson]
+   * @param {string} [options.contactDetails]
    * @param {string} [options.contactVisibility] // One of ContactVisibility
-   * @param {string} [options.duration] // e.g. '1h_or_less', '1_to_2', etc.
    * @param {number} [options.costIntroductory]
    * @param {number} [options.costRegular]
-   * @param {boolean} [options.directContact] // true = direct, false = via website
+   * @param {string} [options.size] // One of EventSize
+   * @param {boolean} [options.directContact]
+   * @param {string|null} [options.addedBy]
+   * @param {Date|null} [options.addedAt]
+   * @param {Date|null} [options.lastEdited]
    * @param {string[]} [options.registeredUsers]
    * @param {string[]} [options.interestedUsers]
-   * @param {boolean} [options.isCancelled] // Event is cancelled
-   * @param {boolean} [options.isDeleted] // Event is deleted
-   * @param {string} [options.size] // Event size, one of EventSize
+   * @param {boolean} [options.isCancelled]
+   * @param {boolean} [options.isDeleted]
    */
   constructor({
     eventId,
@@ -72,29 +78,33 @@ class Event {
     isDeleted = false,
   }) {
     this.eventId = eventId;
-    this.addedBy = addedBy;
-    this.date = date;
     this.title = title;
-    this.locationDescription = locationDescription;
+    this.description = description;
+    this.date = date;
+    this.startTime = startTime;
+    this.duration = duration;
+    this.locationAddress = locationAddress;
+    this.locationPostcode = locationPostcode;
     this.location = location;
     this.memberOnly = memberOnly;
     this.externalRegister = externalRegister;
     this.localMeetRegister = localMeetRegister;
     this.groupTags = groupTags;
     this.categoryTags = categoryTags;
-    this.description = description;
     this.contactPerson = contactPerson;
     this.contactDetails = contactDetails;
     this.contactVisibility = contactVisibility;
-    this.duration = duration;
     this.costIntroductory = costIntroductory;
     this.costRegular = costRegular;
-    this.directContact = directContact;
+    this.size = size;
+    // System fields
+    this.addedBy = addedBy;
+    this.addedAt = addedAt;
+    this.lastEdited = lastEdited;
     this.registeredUsers = registeredUsers;
     this.interestedUsers = interestedUsers;
     this.isCancelled = isCancelled;
     this.isDeleted = isDeleted;
-    this.size = size;
     // System fields
   }
 }
